216e28fa86ca3c5b0a3a31411d32dbc46c456600
Unknown <travisdavis_p@outlook.com>
2018-04-17 21:11:33 -0400

    Input testing in python

--- Scripts/Player.py
+++ Scripts/Player.py
@@ -6,13 +6,22 @@ from godot.bindings import *
 class Player(KinematicBody2D):
 	
 	def _enter_tree(self):
+		self._pos = self.get_position()
 		pass
 		
 	def _ready(self):
 		pass
 	
 	def _process(self,delta):
-		pass
+		#print(delta)
+		if Input.is_action_pressed('Player_Up'):
+			print(f'UP was Pressed and Caught by {self}')
+		elif Input.is_action_pressed('Player_Down'):
+			print(f'DOWN was Pressed and Caught By {self}')
+		if Input.is_action_pressed('Player_Left'):
+			print(f'LEFT was Pressed and Caught By {self}')
+		elif Input.is_action_pressed('Player_Right'):
+			print(f'RIGHT was Pressed and Caught By {self}')
 		
 	def _physics_process(self,delta):
 		pass
@@ -22,4 +31,7 @@ class Player(KinematicBody2D):
 	
 	def _exit_tree(self):
 		pass
-  
+  #####Data Model#####
+	def __repr__(self):
+		return f'Player at (x:{self._pos.x},y:{self._pos.y})'
+	
