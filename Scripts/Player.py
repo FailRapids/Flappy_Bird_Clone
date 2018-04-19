e07998fa27201340dc29422a1c14b05d48be38ad
Unknown <travisdavis_p@outlook.com>
2018-04-18 22:55:27 -0400

    Python Verison

--- Scripts/Player.py
+++ Scripts/Player.py
@@ -1,164 +1,35 @@
 from godot import exposed, export
 from godot.bindings import *
-
+from godot.node_path import NodePath
+from godot.vector2 import Vector2
 
 @exposed
 class Player(KinematicBody2D):
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<<
-	
-	def _enter_tree(self):
-		self._pos = self.get_position()
-		pass
-		
-	def _ready(self):
-		pass
-	
-	def _process(self,delta):
-		#print(delta)
-		if Input.is_action_pressed('Player_Up'):
-			print(f'UP was Pressed and Caught by {self}')
-		elif Input.is_action_pressed('Player_Down'):
-			print(f'DOWN was Pressed and Caught By {self}')
-		if Input.is_action_pressed('Player_Left'):
-			print(f'LEFT was Pressed and Caught By {self}')
-		elif Input.is_action_pressed('Player_Right'):
-			print(f'RIGHT was Pressed and Caught By {self}')
-		
-	def _physics_process(self,delta):
-		pass
-		
-	def _input(self,event):
-		pass
-	
-	def _exit_tree(self):
-		pass
-  #####Data Model#####
-	def __repr__(self):
-		return f'Player at (x:{self._pos.x},y:{self._pos.y})'
-	
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
-    _STATES = ["IDLE","MOVE"]
-    def _enter_tree():
-        self._max_walk = 450 #pix
-        self._max_run = 700 #pix
-        
+    _GRAVITY_VEC = Vector2(0,900)#pix/sec
+    _JUMP_SPEED = -480
+    _linear_vel = Vector2(0,0)
+    _time = 0
+    def _enter_tree(self):
+        pass
+
     def _ready(self):
-        self._state = 0
-        
+        pass
 
     def _process(self,delta):
-        pass
-    
+#        self._time += delta
+#        if self._time > 5:
+#            print(self)
+#            self._time = 0
+#        else:
+#            self._time += delta
+         pass
+
     def _physics_process(self,delta):
-        self._look_direction = self._input_direction  
-        if (self.State == "IDLE") and (self.Input_Direction == Vector2(0,0)):
-            self.State = 1
-        elif self.State == "WALK":
-            #Are We Walking
-            if self.Input_Direction != Vector2(0,0):
-                self.State = 0
-                return
-            self.move(self._max_walk,self.Input_Direction)
-    
-    def move(self, speed, direction):
-	velocity = direction.normalized() * speed
-	self.move_and_slide(velocity, Vector2(), 5, 2)
-    
-    @property
-    def Input_Direction(self):
-        self._input_direction = Vector2(0,0)
-        if Input.is_action_pressed("Player_Up"):
-            self._input_direction = Vector2(0,-1)
-        elif Input.is_action_pressed("Player_Down"):
-            self._input_direction = Vector2(0,1)
-        elif Input.is_action_pressed("Player_Left"):
-            self._input_direction = Vector2(-1,0)
-        elif Input.is_action_pressed("Player_Right"):
-            self._input_direction = Vector2(1,0)
-        return self._input_direction
+        #UGH MATH
+        self._linear_vel += self._GRAVITY_VEC * delta
+        self._linear_vel = self.move_and_slide(self._linear_vel,Vector2(0,-1),0,4,0.78)
+        if Input.is_action_just_pressed("Player_Jump"):
+            self._linear_vel.y = self._JUMP_SPEED
 
-    @property
-    def Look_Direction(self)
-        return self._look_direction
-    
-    @Look_Direction.setter
-    def Look_Direction(self,direction):
-        if isinstance(direction,Vector2):
-            self._look_direction = direction.normalized()
-        self.get_node(NodePath("BodyPivot")).set_scale(Vector2(direction.x,1))
-                      
-    @property
-    def State(self):
-        return self._state
-
-    @State.setter
-    def State(self value:int=0)
-        if value < 2 and value >=0:
-            self._state = _STATES[value]
-            self.get_node("AnimationPlayer").play(self._state)
-        else:
-            raise ValueError("{self._STATES}")
-=======
-	
-	def _enter_tree(self):
-		self._pos = self.get_position()
-		pass
-		
-	def _ready(self):
-		pass
-	
-	def _process(self,delta):
-		#print(delta)
-		if Input.is_action_pressed('Player_Up'):
-			print(f'UP was Pressed and Caught by {self}')
-		elif Input.is_action_pressed('Player_Down'):
-			print(f'DOWN was Pressed and Caught By {self}')
-		if Input.is_action_pressed('Player_Left'):
-			print(f'LEFT was Pressed and Caught By {self}')
-		elif Input.is_action_pressed('Player_Right'):
-			print(f'RIGHT was Pressed and Caught By {self}')
-		
-	def _physics_process(self,delta):
-		pass
-		
-	def _input(self,event):
-		pass
-	
-	def _exit_tree(self):
-		pass
-<<<<<<< HEAD
-  
->>>>>>> parent of 216e28f... Input testing in python
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
->>>>>>>
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
-  #####Data Model#####
-	def __repr__(self):
-		return f'Player at (x:{self._pos.x},y:{self._pos.y})'
-	
->>>>>>> build_player
+    def __repr__(self):
+        return f'Player:{self.get_position()}:{self._linear_vel.y}:{self._JUMP_SPEED}'
