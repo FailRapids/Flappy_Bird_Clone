9bdab5052167edc16e0773e08a3b58e051a396ee
Unknown <travisdavis_p@outlook.com>
2018-04-19 20:11:35 -0400

    ADDed Some Debug Controls
    A = go left
    D = go right
    Mostly just for moveing around scence
    for auto move just set Force_Grav on player to a non 0 x

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,9 +7,16 @@ var _linear_vel = Vector2()
 func _physics_process(delta):
 	#calc New force
 	_linear_vel += delta * Force_Grav
-	#apply Grav
-	_linear_vel = move_and_slide(_linear_vel)
 	if Input.is_action_just_pressed("Player_Jump"):
 		#apply force of Jump rember godot y-axis is inverted
 		_linear_vel.y = -Jump_Speed
 		$AnimationPlayer.play("Flap",-1,1.0,false)
+	else:#TODO Check if Failing Before 
+		set_rotation(clamp(_linear_vel.angle(),0,35))
+
+	if OS.is_debug_build():
+		if Input.is_action_just_pressed("Player_Right"):
+			self.Force_Grav.x = 100
+		if Input.is_action_just_pressed("Player_Left"):
+			self.Force_Grav.x = -100
+	_linear_vel = move_and_slide(_linear_vel)
