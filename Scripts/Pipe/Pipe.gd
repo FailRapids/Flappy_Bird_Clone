0e42e8b035bd37e9479fb568297eec8b2d15ee25
Unknown <travisdavis_p@outlook.com>
2018-05-07 10:24:13 -0400

    Playable State

--- Scripts/Pipe/Pipe.gd
+++ Scripts/Pipe/Pipe.gd
@@ -4,7 +4,7 @@ export(int) var speed = 20
 
 
 func _ready():
-	set_position(Vector2(0,-(randi() % 200)))
+	set_position(Vector2(0,-(randi() % 300)))
 	get_parent().get_parent().connect("body_exited",self,"_on_World_body_exited")
 
 func _physics_process(delta):
--- Scripts/Pipe/Spawn.gd
+++ Scripts/Pipe/Spawn.gd
@@ -6,7 +6,6 @@ export(PackedScene) var Pipe
 func _on_Timer_timeout():
 	var new_pipe = Pipe.instance()
 	add_child(new_pipe)
-	print("spawn")
 
 
 func start():
